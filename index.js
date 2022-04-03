5d09dd97ed98a77fc66fbff67158d99df6c6d6cf
RayhanAnandhias <48201126+RayhanAnandhias@users.noreply.github.com>
2022-04-03 11:55:38 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const FileSystem = require('fs');
+const lineByLine = require('n-readlines');
+const liner = new lineByLine('./data-soal/Psychology.txt');
+
+let line;
+let idQuestion = 0;
+let arrQuestion = [];
+let objQuestion;
+
+while ((line = liner.next())) {
+  let lineString = line.toString('utf-8').trim();
+  let firstChar = lineString.charAt(0);
+  if (firstChar !== '0' && firstChar !== '1' && firstChar !== '') {
+    idQuestion++;
+    objQuestion = {
+      id: idQuestion,
+      question: lineString,
+      option: [],
+      answer: null
+    };
+    arrQuestion.push(objQuestion);
+  }
+  if (firstChar === '0' || firstChar === '1') {
+    lineString = lineString.slice(2);
+    arrQuestion[idQuestion - 1].option.push(lineString);
+    if (firstChar === '1') {
+      arrQuestion[idQuestion - 1].answer = lineString;
+    }
+  }
+}
+
+FileSystem.writeFile(
+  './data-soal/psychology.json',
+  JSON.stringify(arrQuestion),
+  (err) => {
+    if (err) throw err;
+  }
+);
